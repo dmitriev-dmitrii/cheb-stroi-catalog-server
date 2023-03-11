a5270c09beb9dfe6355136a865b163f49b7f96f4
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-03-11 17:46:21 +0300

    create base controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { BrandsModule } from './brands/brands.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ ProductsModule, BrandsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
