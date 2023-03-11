95636002cbf997e6b8d6832b8589707bbc6226dd
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-03-11 18:40:36 +0300

    remove default app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ProductsModule } from './products/products.module';
 import { BrandsModule } from './brands/brands.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [ ProductsModule, BrandsModule, CategoriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api/catalog/')
   await app.listen(3000)
-  console.log('http://localhost:3000/api');
+  console.log('http://localhost:3000/api/catalog/');
 }
 bootstrap();
